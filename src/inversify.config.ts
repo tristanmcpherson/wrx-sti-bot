e6bb01da7e86a2e9a8ebb05a1d39a51448bd61c9
tristanmcpherson <shredder8910@gmail.com>
2021-05-28 03:57:49 -0400

    Fix file loading

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,6 +1,7 @@
 import { readdirSync } from "fs";
 import { Container } from "inversify";
 import { buildProviderModule } from "inversify-binding-decorators";
+import path from "path";
 
 // god help us all
 // container must be create and exported first
@@ -8,11 +9,11 @@ const container = new Container();
 export default container;
 
 // then we must manually import the commands
-const commandDir = __dirname + "\\commands";
+const commandDir = path.join(__dirname, "commands");
 const files = readdirSync(commandDir);
-files.filter(f => f.endsWith(".js")).forEach(f => {
-    require(`${commandDir}\\${f}`);
-})
+files.filter(file => file.endsWith(".js")).forEach(file => {
+    require(path.join(commandDir, file));
+});
 
 // then we build the provider module and load it into the container
 container.load(buildProviderModule());
