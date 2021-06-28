8fe86043022dde19697235ad035e1dd06c8d51e9
Justin Sacbibit <sacbibit@yelp.com>
2021-06-28 13:58:10 -0700

    Add temperature handler

---
+++ src/commands/temperature.ts
@@ -0,0 +1,33 @@
+import { Client, Message } from "discord.js";
+import { provide } from "inversify-binding-decorators";
+import { IMessageHandler, MessageHandler } from "../models/messageManager";
+
+const temperatureRegex = /(\d+) ?(f|c)/i;
+
+@provide(MessageHandler)
+class TemperatureHandler implements IMessageHandler {
+    async shouldHandle(message: Message, client: Client): Promise<boolean> {
+        return message.author.id !== client.user!.id && temperatureRegex.test(message.content);
+    }
+
+    async handler(message: Message) {
+        const regexResult = temperatureRegex.exec(message.content);
+        if (regexResult === null) {
+            return;
+        }
+
+        const value = parseFloat(regexResult[1]);
+        const unit = regexResult[2].toUpperCase();
+
+        let newValue, newUnit;
+        if (unit === 'F') {
+            newValue = (value - 32) * 5/9;
+            newUnit = 'C';
+        } else {
+            newValue = value * 9/5 + 32;
+            newUnit = 'F';
+        }
+
+        await message.reply(`${value.toFixed(2)}°${unit} ~ ${newValue?.toFixed(2)}°${newUnit}`);
+    }
+}
