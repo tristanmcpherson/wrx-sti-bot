8c862ee99cf1c551d2de15dd8432af5ec701b951
Justin Sacbibit <sacbibit@yelp.com>
2021-06-30 13:31:13 -0700

    Make 510 command trigger less

--- src/commands/510.ts
+++ src/commands/510.ts
@@ -2,10 +2,12 @@ import { Client, Message } from "discord.js";
 import { provide } from "inversify-binding-decorators";
 import { IMessageHandler, MessageHandler } from "../models/messageManager";
 
+const fiveTenRegex = /510([^\d]|$)/;
+
 @provide(MessageHandler)
 class FiveTenHandler implements IMessageHandler {
     async shouldHandle(message: Message, client: Client): Promise<boolean> {
-        return message.author.id !== client.user!.id && message.content.includes('510');
+        return message.author.id !== client.user!.id && fiveTenRegex.test(message.content);
     }
 
     async handler(message: Message) {
