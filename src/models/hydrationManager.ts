202cc99ce3b715901d89ba971986a137933c69d1
Justin Sacbibit <sacbibit@yelp.com>
2021-06-30 14:15:12 -0700

    .

--- src/models/hydrationManager.ts
+++ src/models/hydrationManager.ts
@@ -10,12 +10,15 @@ export class HydrationManager {
         this._channelId = channelId;
         this._roleId = roleId;
 
-        setInterval(async () => {
-            const hour = new Date().getHours();
-            if (hour >= 16 || hour < 4) {
-                const channel = await this._client.channels.fetch(this._channelId) as TextChannel;
-                await channel.send(`@<&${this._roleId}> drink water u retards`)
-            }
-        }, 60 * 60 * 1000);
+        this.hydrate();
+        setInterval(this.hydrate, 60 * 60 * 1000);
+    }
+
+    async hydrate() {
+        const hour = new Date().getHours();
+        if (hour >= 16 || hour < 4) {
+            const channel = await this._client.channels.fetch(this._channelId) as TextChannel;
+            await channel.send(`@<&${this._roleId}> drink water u retards`)
+        }
     }
 }
